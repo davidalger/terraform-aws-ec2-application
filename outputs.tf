a575ba79299c1fb5fa9c0f9ccde475e10e55e385
David Alger <davidmalger@gmail.com>
2020-04-20 12:13:39 -0500

    Added instance ID and ARN to list of outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
+output "instance_id" {
+  value = aws_instance.default.id
+}
+
+output "instance_arn" {
+  value = aws_instance.default.arn
+}
+
 output "instance_name" {
   value = aws_instance.default.tags["Name"]
 }
